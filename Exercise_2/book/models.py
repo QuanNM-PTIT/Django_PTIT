5a74d1f941396f3559eb536b12ce8d91f464b57d
Jim <quannm.ptit@gmail.com>
2024-02-01 11:16:06 +0700

    Ex2 - Initial

---
+++ Exercise_2/book/models.py
@@ -0,0 +1,14 @@
+from djongo import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Books'
+        ordering = ['created_at']
---
+++ Exercise_2/cart/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class CartItem(models.Model):
+    cart_id = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    book_id = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'CartItems'
+        ordering = ['created_at']
+
+
+class Cart(models.Model):
+    cart_id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Carts'
+        ordering = ['created_at']
