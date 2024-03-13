519f85f5ca7edfb808dd681b37b825cb1b6ad40a
Jim <quannm.ptit@gmail.com>
2024-03-13 21:13:30 +0700

    Add static folder and HTML files

--- Exercise_2/catalog/apps.py
+++ Exercise_2/catalog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BookConfig(AppConfig):
+class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'book'
+    name = 'catalog'
