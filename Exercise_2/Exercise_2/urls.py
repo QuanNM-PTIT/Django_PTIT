f0fa61b3d89bb4d8bd66c1eab23856d7132c1d98
Jim <quannm.ptit@gmail.com>
2024-03-13 21:16:33 +0700

    Add static folder and HTML files

--- Exercise_2/Exercise_2/urls.py
+++ Exercise_2/Exercise_2/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from book.views import add_book
+from catalog.views import product_list
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add_book/', add_book, name='add_book'),
-]
+    path('products/', product_list, name='product_list'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Exercise_2/catalog/database_router.py
+++ Exercise_2/catalog/database_router.py
@@ -1,20 +1,20 @@
-class BookDatabaseRouter:
+class CatalogDatabaseRouter:
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'book':
+        if model._meta.app_label == 'catalog':
             return 'mongodb'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'book':
+        if model._meta.app_label == 'catalog':
             return 'mongodb'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label == 'book' or obj2._meta.app_label == 'book':
+        if obj1._meta.app_label == 'catalog' or obj2._meta.app_label == 'catalog':
             return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'book':
+        if app_label == 'catalog':
             return db == 'mongodb'
         return None
